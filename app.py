ef0c36bdac2d4db44f1c7343e1bd77bac363a52b
ibarbylev <ibarbylev@gmail.com>
2022-12-08 00:08:14 +0200

    change ip

--- app.py
+++ app.py
@@ -168,7 +168,7 @@ async def handler(websocket):
 
 
 async def main():
-    async with websockets.serve(handler, "151.248.122.102", 8001):
+    async with websockets.serve(handler, "", 8001):
         await asyncio.Future()  # run forever
 
 
