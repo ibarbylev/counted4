038376c858c7c39b4c554da1490faac6f60d91d8
ibarbylev <ibarbylev@gmail.com>
2022-12-07 02:37:52 +0200

    change ws address

--- main.js
+++ main.js
@@ -76,7 +76,7 @@ window.addEventListener("DOMContentLoaded", () => {
   const board = document.querySelector(".board");
   createBoard(board);
   // Open the WebSocket connection and register event handlers.
-  const websocket = new WebSocket("ws://localhost:8001/");
+  const websocket = new WebSocket("ws://151.248.122.102:8000/");
   initGame(websocket);
   receiveMoves(board, websocket);
   sendMoves(board, websocket);
